1db6ce595948a5790313f3def209eb0f3284b26c
Nick <manelliproductions@gmail.com>
2020-04-25 12:27:09 -0400

    Added fullscreen functionality to the Display.java file

--- src/com/harmony/game/graphics/Display.java
+++ src/com/harmony/game/graphics/Display.java
@@ -2,17 +2,15 @@ package com.harmony.game.graphics;
 
 import com.harmony.game.Game;
 import com.harmony.game.state.GameStateManager;
+import com.harmony.game.state.SettingsState;
 import com.harmony.game.utils.ImageUtils;
+import com.harmony.game.utils.Input;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.*;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
-import java.util.Map;
 
 public class Display {
 
@@ -25,6 +23,9 @@ public class Display {
     public static float absHeight;
 
     private static Dimension d;
+
+    private static GraphicsDevice device;
+    private static DisplayMode displayMode;
 
     private static JFrame frame;
     private static BufferedImage image;
@@ -48,11 +49,18 @@ public class Display {
         canvas = new Canvas();
         canvas.setPreferredSize(new Dimension(width, height));
 
-        if(System.getProperty("os.name").equals("Mac OS X")) {
-            frame = new JFrame();
-        } else {
-            frame = new JFrame(title);
-        }
+//        if(System.getProperty("os.name").equals("Mac OS X")) {
+//            frame = new JFrame();
+//        } else {
+//            frame = new JFrame(title);
+//        }
+
+        frame = new JFrame(title);
+
+        GraphicsEnvironment environment = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        device = environment.getDefaultScreenDevice();
+
+        displayMode = device.getDisplayMode();
 
         frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 
@@ -69,12 +77,12 @@ public class Display {
         g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
         g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-        if(System.getProperty("os.name").equals("Mac OS X")) {
-            SwingUtilities.invokeLater(() -> {
-                frame.getRootPane().putClientProperty("apple.awt.fullWindowContent", true);
-                frame.getRootPane().putClientProperty("apple.awt.transparentTitleBar", true);
-            });
-        }
+//        if(System.getProperty("os.name").equals("Mac OS X")) {
+//            SwingUtilities.invokeLater(() -> {
+//                frame.getRootPane().putClientProperty("apple.awt.fullWindowContent", true);
+//                frame.getRootPane().putClientProperty("apple.awt.transparentTitleBar", true);
+//            });
+//        }
 
         frame.setVisible(true);
 
@@ -100,7 +108,7 @@ public class Display {
         frame.addComponentListener(new ComponentAdapter() {
             @Override
             public void componentResized(ComponentEvent e) {
-                if(frame.getWidth() <= 0 || frame.getHeight() <= 0) return;
+                if (frame.getWidth() <= 0 || frame.getHeight() <= 0) return;
                 absWidth = frame.getWidth();
                 absHeight = frame.getHeight();
                 canvas.createBufferStrategy(2);
@@ -120,10 +128,58 @@ public class Display {
     }
 
     public static void update() {
+        if(SettingsState.isFullscreen && Input.isKeyDown(KeyEvent.VK_ESCAPE)) {
+            SettingsState.isFullscreen = false;
+            setFullScreen(false);
+        }
+
         try {
             g.drawImage(image, 0, 0, (int) absWidth, (int) absHeight, null);
             bs.show();
         } catch(Exception e) {}
+    }
+
+    public static void setFullScreen(boolean state) {
+        if(System.getProperty("os.name").equals("Mac OS X")) {
+            if (state) {
+                System.out.println("### SETTING: Entering Full Screen ###");
+                device.setFullScreenWindow(frame);
+                frame.setSize(Toolkit.getDefaultToolkit().getScreenSize());
+
+                if (displayMode != null && device.isDisplayChangeSupported()) {
+                    try {
+                        device.setDisplayMode(displayMode);
+                    } catch (Exception e) {
+                        System.err.println("Crystals - Display Mode Error Entering Fullscreen: Display Class");
+                        e.printStackTrace();
+                    }
+                }
+            } else {
+                System.out.println("### SETTING: Exiting Full Screen ###");
+                Window fullScreenWindow = device.getFullScreenWindow();
+                if (fullScreenWindow != null) fullScreenWindow.dispose();
+                device.setFullScreenWindow(null);
+                frame.setVisible(true);
+            }
+        } else {
+            if(state) {
+                frame.dispose();
+                frame.setUndecorated(true);
+                frame.setAlwaysOnTop(true);
+                Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+                d.width += 16;                              d.height += 8;
+                frame.setSize(d);
+                frame.setLocationRelativeTo(null);
+                frame.setVisible(true);
+            } else {
+                frame.dispose();
+                frame.setUndecorated(false);
+                frame.setAlwaysOnTop(false);
+                frame.setSize(d);
+                frame.setLocationRelativeTo(null);
+                frame.setVisible(true);
+            }
+        }
     }
 
     public static void setCursor(int type) { frame.setCursor(new Cursor(type)); }
