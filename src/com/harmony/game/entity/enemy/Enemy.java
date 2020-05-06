9a3141345527592d813b42c419bbcbf571311c80
Nick <manelliproductions@gmail.com>
2020-05-06 15:47:24 -0400

    Added in enemy-player pathfinding AI

--- src/com/harmony/game/entity/enemy/Enemy.java
+++ src/com/harmony/game/entity/enemy/Enemy.java
@@ -9,13 +9,20 @@ import com.harmony.game.state.chapters.Chapter;
 import com.harmony.game.tiles.ObjectTileMap;
 import com.harmony.game.utils.Vector2f;
 
+import java.awt.*;
+
 public abstract class Enemy extends Entity  {
 
-    protected Player player;
+    protected final Player player;
 
     protected BoxCollider detectionCollider;
 
-    private Chapter chapter;
+    private final Chapter chapter;
+
+    protected int ANIMATION_RIGHT = 0;
+    protected int ANIMATION_LEFT  = 1;
+    protected int ANIMATION_UP    = 2;
+    protected int ANIMATION_DOWN  = 3;
 
     public Enemy(Vector2f position, Chapter chapter, Player player, ObjectTileMap objectTileMap, int width, int height) {
         super(position, objectTileMap, width, height);
@@ -26,6 +33,48 @@ public abstract class Enemy extends Entity  {
 
     private void playHitEffect() {
         if(getClass() == Slime.class) Slime.slimeEffect.play();
+    }
+
+    protected void playerPathfinderAI() {
+
+        up    = false;
+        down  = false;
+        right = false;
+        left  = false;
+
+        dx = 0;
+        dy = 0;
+
+        if(detectionCollider.collisionPlayer(player)) {
+
+            // Determine what direction the enemy will move
+            if(Math.floor(player.position.y + player.getBoxCollider().getOffset().y - position.getWorldPosition().y) > 0) {
+                down = true;
+                currentAnimation = ANIMATION_DOWN;
+            } else if(Math.floor(player.position.y + player.getBoxCollider().getOffset().y - position.getWorldPosition().y) < 0) {
+                up = true;
+                currentAnimation = ANIMATION_UP;
+            }
+
+            if(Math.floor(player.position.x + player.getBoxCollider().getOffset().x - position.getWorldPosition().x) > 0) {
+                right = true;
+                currentAnimation = ANIMATION_RIGHT;
+            } else if(Math.floor(player.position.x + player.getBoxCollider().getOffset().x - position.getWorldPosition().x) < 0) {
+                left = true;
+                currentAnimation = ANIMATION_LEFT;
+            }
+
+            // Move the enemy in the set direction
+
+            if(left)  dx = Math.max(-maxMoveSpeed, player.position.x + player.getBoxCollider().getOffset().x - position.getWorldPosition().x);
+            if(up)    dy = Math.max(-maxMoveSpeed, player.position.y + player.getBoxCollider().getOffset().y - position.getWorldPosition().y);
+            if(down)  dy = Math.min(maxMoveSpeed, player.position.y + player.getBoxCollider().getOffset().y - position.getWorldPosition().y);
+            if(right) dx = Math.min(maxMoveSpeed, player.position.x + player.getBoxCollider().getOffset().x - position.getWorldPosition().x);
+
+            isIdle = false;
+        } else {
+            isIdle = true;
+        }
     }
 
     @Override
@@ -54,16 +103,28 @@ public abstract class Enemy extends Entity  {
         if (!boxCollider.collisionTile(objectTileMap, 0, dy)) {
             position.y += dy;
         }
+    }
 
+    @Override
+    public void draw(Graphics2D g) {
+        if(Game.debugMode) {
+            g.setColor(Color.BLUE);
+            g.drawRect((int) (position.getWorldPosition().x + boxCollider.getOffset().x), (int) (position.getWorldPosition().y + boxCollider.getOffset().y),
+                    boxCollider.getWidth(), boxCollider.getHeight());
+
+            g.setColor(Color.RED);
+            g.drawRect((int) (position.getWorldPosition().x + detectionCollider.getOffset().x), (int) (position.getWorldPosition().y + detectionCollider.getOffset().y),
+                    detectionCollider.getWidth(), detectionCollider.getHeight());
+        }
     }
 
     @Override
     public void hit(int damage) {
         if(damage > 0)
             super.hit(damage);
-        if(player.position.y <= position.getWorldPosition().y) dy += 130;
-        else if(player.position.y > position.getWorldPosition().y)  dy -= 130;
-        if(player.position.x <= position.getWorldPosition().x) dx += 130;
-        else if(player.position.x > position.getWorldPosition().x)  dx -= 130;
+        if(player.position.y <= position.getWorldPosition().y) dy += 100;
+        else if(player.position.y > position.getWorldPosition().y)  dy -= 100;
+        if(player.position.x <= position.getWorldPosition().x) dx += 100;
+        else if(player.position.x > position.getWorldPosition().x)  dx -= 100;
     }
 }
--- src/com/harmony/game/entity/enemy/Slime.java
+++ src/com/harmony/game/entity/enemy/Slime.java
@@ -1,9 +1,8 @@
 package com.harmony.game.entity.enemy;
 
-import com.harmony.game.Game;
+import com.harmony.game.animation.Animation;
 import com.harmony.game.audio.AudioClip;
 import com.harmony.game.entity.Player;
-import com.harmony.game.animation.Animation;
 import com.harmony.game.graphics.Camera;
 import com.harmony.game.graphics.Sprite;
 import com.harmony.game.item.Drops;
@@ -18,25 +17,19 @@ public class Slime extends Enemy {
 
     public static final AudioClip slimeEffect = new AudioClip("/audio/entity/slime/slime_hurt.wav");
 
-    public static final int ANIMATION_RIGHT = 0;
-    public static final int ANIMATION_LEFT  = 1;
-    public static final int ANIMATION_UP    = 2;
-    public static final int ANIMATION_DOWN  = 3;
-
     public Slime(Vector2f position, Chapter chapter, Player player, ObjectTileMap objectTileMap, int width, int height) {
         super(position, chapter, player, objectTileMap, width, height);
     }
 
     @Override
     public void onCreate() {
-        boxCollider = new BoxCollider(this, new Vector2f(), width, height);
+        boxCollider = new BoxCollider(this, new Vector2f(0, 20), width, height - 20);
         detectionCollider = new BoxCollider(this, new Vector2f(-200, -200), width + 400, height + 400);
 
         sprite = new Sprite("/entity/enemy/slime.png", 18, 18);
         animation = new Animation(sprite);
 
-        maxMoveSpeed = 2f;
-        acceleration = 1.5f;
+        maxMoveSpeed = 1.5f;
         health = maxHealth = 4;
         damage = 1;
 
@@ -48,49 +41,13 @@ public class Slime extends Enemy {
     public void update() {
         if(!Camera.shouldHandleEntity(this)) return;
 
-        left = false;
-        right = false;
-        up = false;
-        down = false;
-
-        if(player.getBoxCollider().getBoundsAsAbsRect().intersects(detectionCollider.getBoundsAsRelativeRect())) {
-            Vector2f pos = player.position.add(player.getBoxCollider().getOffset()).sub(position.getWorldPosition()).inverse();
-
-            if(pos.x > 0) {
-                dx = Math.min(maxMoveSpeed, pos.x);
-                right = true;
-            } else if(pos.x < 0) {
-                dx = Math.max(-maxMoveSpeed, pos.x);
-                left = true;
-            }
-
-            if(pos.y > 0) {
-                dy = Math.min(maxMoveSpeed, pos.y);
-                down = true;
-            } else if(pos.y < 0) {
-                dy = Math.max(-maxMoveSpeed, pos.y);
-                up = true;
-            }
-        } else {
-            isIdle = true;
-        }
-
-        if(!isIdle) {
-            if(left) currentAnimation = ANIMATION_LEFT;
-            else if(right) currentAnimation = ANIMATION_RIGHT;
-            else if(up) currentAnimation = ANIMATION_UP;
-            else if(down) currentAnimation = ANIMATION_DOWN;
-        }
-
-        isIdle = (Math.ceil(dx) == 0 && Math.ceil(dy) == 0) || (Math.floor(dx) == 0 && Math.floor(dy) == 0);
-
+        super.playerPathfinderAI();
         super.update();
     }
 
     @Override
     public void draw(Graphics2D g) {
         if(!Camera.shouldHandleEntity(this)) return;
-
         if(!isIdle) {
             g.drawImage(animation.animate(currentAnimation, 100), (int) position.getWorldPosition().x,
                     (int) position.getWorldPosition().y, width, height, null);
@@ -99,15 +56,7 @@ public class Slime extends Enemy {
                     (int) position.getWorldPosition().y, width, height, null);
         }
 
-        if(Game.debugMode) {
-            g.setColor(Color.BLUE);
-            g.drawRect((int) (position.getWorldPosition().x + boxCollider.getOffset().x), (int) (position.getWorldPosition().y + boxCollider.getOffset().y),
-                    boxCollider.getWidth(), boxCollider.getHeight());
-
-            g.setColor(Color.RED);
-            g.drawRect((int) (position.getWorldPosition().x + detectionCollider.getOffset().x), (int) (position.getWorldPosition().y + detectionCollider.getOffset().y),
-                    detectionCollider.getWidth(), detectionCollider.getHeight());
-        }
+        super.draw(g);
     }
 
     @Override
