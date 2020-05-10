0f5895c3b185ce89f487a8752e45c6067f3df68c
Nick <manelliproductions@gmail.com>
2020-05-09 20:20:59 -0400

    Did some code refactoring to optimize the player experience.

--- src/com/harmony/game/state/chapters/Chapter3.java
+++ src/com/harmony/game/state/chapters/Chapter3.java
@@ -1,6 +1,5 @@
 package com.harmony.game.state.chapters;
 
-import com.harmony.game.audio.BackgroundAmbience;
 import com.harmony.game.entity.enemy.Slime;
 import com.harmony.game.graphics.Camera;
 import com.harmony.game.graphics.ConsoleMessage;
@@ -8,11 +7,14 @@ import com.harmony.game.item.Drops;
 import com.harmony.game.item.Item;
 import com.harmony.game.object.Building;
 import com.harmony.game.object.Chest;
+import com.harmony.game.object.NextLevelInvisible;
 import com.harmony.game.utils.Vector2f;
 
 import java.awt.*;
 
 public class Chapter3 extends Chapter {
+
+    private NextLevelInvisible nextLevelInvisible;
 
     public Chapter3() {
         super("/tile/places/forest-c3.tmx");
@@ -30,6 +32,11 @@ public class Chapter3 extends Chapter {
 
 //        BackgroundAmbience.playBackgroundAudio(BackgroundAmbience.OUTSIDE_AMBIENCE);
 
+        message = new ConsoleMessage(console, "I think there's one of those slimes up ahead...~Use the space-bar while facing " +
+                "the slime to attack.~Good Luck...", null);
+
+        message.run();
+
         super.addGameObject(new Building(new Vector2f(3380, 2574), Building.Type.HUT));
         super.addGameObject(new Building(new Vector2f(3309, 1560), Building.Type.VILLA));
 
@@ -42,6 +49,9 @@ public class Chapter3 extends Chapter {
         super.enemies.add(new Slime(new Vector2f(6212, 885), this, player, tileManager.getObjectsMap(),
                 64, 64));
 
+        super.enemies.add(new Slime(new Vector2f(5605, 6565), this, player, tileManager.getObjectsMap(),
+                64, 64));
+
         Drops.drop(new Vector2f(5170, 805), Drops.DROP_HEALTH_POINT);
         Drops.drop(new Vector2f(3076, 830), Drops.DROP_HEALTH_POINT);
 
@@ -49,12 +59,8 @@ public class Chapter3 extends Chapter {
             chest.setItem(Item.CURRENCY_10);
         }
 
-        super.immobilizeEntities = false;
-
-        message = new ConsoleMessage(console, "I think there's one of those slimes up ahead...~Use the space-bar while facing " +
-                "the slime to attack.~Good Luck...", null);
-
-        message.run();
+        nextLevelInvisible = new NextLevelInvisible(new Vector2f(4952, 7498), player, 400, 32);
+        nextLevelInvisible.shouldDetect = true;
     }
 
     @Override
@@ -64,11 +70,14 @@ public class Chapter3 extends Chapter {
 //        player.printPosition();
 
         message.update();
+        nextLevelInvisible.update();
     }
 
     @Override
     public void draw(Graphics2D g) {
         super.draw(g);
+
+        nextLevelInvisible.draw(g);
     }
 
     @Override
