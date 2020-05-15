37241b95c52ba138a01a704c415203ae196679a9
Nick <manelliproductions@gmail.com>
2020-05-15 15:08:00 -0400

    Updated the starting camera position

--- src/com/harmony/game/state/chapters/Chapter4.kt
+++ src/com/harmony/game/state/chapters/Chapter4.kt
@@ -1,18 +1,19 @@
 package com.harmony.game.state.chapters
 
-import com.harmony.game.entity.enemy.Spider
 import com.harmony.game.graphics.Camera
 import com.harmony.game.utils.Vector2f
 
-class Chapter4 : Chapter("/tile/places/forest-c3.tmx") {
+class Chapter4 : Chapter("/tile/places/spider-cave-c4.tmx") {
 
     override fun onCreate() {
         super.onCreate()
 
-        Camera.position = Vector2f(968f, 487f)
+        Camera.position = Vector2f(-448f, 9059f)
         Camera.setDefaultPosition()
+    }
 
-        super.enemies.add(Spider(Vector2f(1568f, 850f), this, player, tileManager.objectsMap))
+    override fun update() {
+        super.update()
     }
 
 }
